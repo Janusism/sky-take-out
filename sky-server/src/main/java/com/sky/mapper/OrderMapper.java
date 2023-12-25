0939525df6961e780e865f297e8b6d9accf98297
xuanyu <3426464304@qq.com>
2023-12-25 10:26:37 +0800

    用户下单功能代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OrderMapper {
+    void insert(Orders orders);
+
+    /**
+     * 批量插入订单明细数据
+     * @param orderDetailList
+     */
+    void insertBatch(List<OrderDetail> orderDetailList);
+}
